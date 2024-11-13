e5f64f437da8bcefc115763d6f5a9e69d6044d6c
Ricardo031 <rjtb31@gmail.com>
2024-11-13 15:38:37 -0500

    Responsive
    Se mejoro el tamaño y ahora sirve en todas las pantallas

--- src/js/url.js
+++ src/js/url.js
@@ -1,1 +1,1 @@
-export const api_lol = 'https://ddragon.leagueoflegends.com/cdn/14.20.1/data/es_ES/champion.json';
+export const api_lol = 'https://ddragon.leagueoflegends.com/cdn/14.22.1/data/es_ES/champion.json';
